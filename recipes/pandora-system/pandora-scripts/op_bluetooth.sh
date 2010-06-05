873d06d71243c062bc321e72466400ebf6eb07ed
Michael Mrozek <EvilDragon@openpandora.de>
2010-06-05 11:15:15 +0200

    op_bluetooth: Added security fixes

--- recipes/pandora-system/pandora-scripts/op_bluetooth.sh
+++ recipes/pandora-system/pandora-scripts/op_bluetooth.sh
@@ -3,30 +3,30 @@
 # Released under the GPL
 
 INTERFACE="`hciconfig | grep "^hci" | cut -d ':' -f 1`"
+LOCK="~/.op_btenabled"
 
-if [ ${1} = "startup" ]; then
-	[ -f ~/.op_btenabled ] && sudo /usr/sbin/hciconfig ${INTERFACE} up pscan && sudo /usr/sbin/bluetoothd ||echo "Bluetooth: User has not enabled Bluetooth." 
+if [ $1 = "startup" ]; then
+	[ -f "$LOCK" ] && sudo /usr/sbin/hciconfig "$INTERFACE" up pscan 1>/dev/null && sudo /usr/sbin/bluetoothd || echo "Bluetooth: User has not enabled Bluetooth." 
+
 else
 	
 	# Figure out if Bluetooth is running or not
 	
-	if [ "`hciconfig ${INTERFACE} | grep UP`" ]
+	if hciconfig "$INTERFACE" | grep UP &>/dev/null
 	then
-		notify-send -u normal "Bluetooth" "Bluetooth is being disabled..." -i /usr/share/icons/hicolor/32x32/apps/blueman.png
-		sudo /usr/sbin/hciconfig ${INTERFACE} down
-		sudo killall btaddconn
-                sudo killall btdelconn
-		rm ~/.op_btenabled
+		notify-send -u normal "Bluetooth" "Bluetooth is being disabled..." -i blueman -t 5000
+		sudo /usr/sbin/hciconfig ${INTERFACE} down 1>/dev/null
+		rm -f "$LOCK"
 	else
 		pgrep bluetoothd
 		echo $INTERFACE
                 if [ $? -ne 1 ]; then
-			notify-send -u normal "Bluetooth" "Bluetooth is being enabled..." -i /usr/share/icons/hicolor/32x32/apps/blueman.png
-			sudo /usr/sbin/hciconfig ${INTERFACE} down
-			sudo /usr/sbin/hciconfig ${INTERFACE} up pscan
-			sudo /usr/sbin/bluetoothd
-			rm ~/.op_btenabled
-			echo true > ~/.op_btenabled
+			notify-send -u normal "Bluetooth" "Bluetooth is being enabled..." -i blueman -t 5000
+			sudo /usr/sbin/hciconfig ${INTERFACE} down 1>/dev/null
+			sudo /usr/sbin/hciconfig ${INTERFACE} up pscan 1>/dev/null
+			sudo /usr/sbin/bluetoothd 1>/dev/null
+			touch "$LOCK"
+
 		fi
 	fi
 fi
