e448773c4d7bf2803e60f6e7f8fc1cf6cec11ff9
Michael Mrozek <EvilDragon@openpandora.de>
2010-06-07 11:27:52 +0200

    Removed Hotfix from GIT

--- recipes/hotfix/2/op_calibrate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TSLIB_CONSOLEDEVICE=none op_runfbapp ts_calibrate
-zenity --info --title="Calibration" --text "The touchscreen has been calibrated. To activate the new settings, you have to reboot your unit."
--- recipes/hotfix/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gksudo ./updater1.sh
