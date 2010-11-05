5ee2667499d0181759ece906ca01e3f23de35571
Michael Mrozek <EvilDragon@openpandora.de>
2010-11-05 01:26:34 +0100

    op_lcdsettings.sh: Added videofilter selection

--- recipes/pandora-system/pandora-scripts/op_lcdsettings.sh
+++ recipes/pandora-system/pandora-scripts/op_lcdsettings.sh
@@ -3,7 +3,7 @@
 # LCD-Settings, v1.0, written by Michael Mrozek aka EvilDragon 2010. Brightness-Settings-Part written by vimacs.
 # This scripts allows you to create, load and save Gamma-Settings and to change the LCD Brightness.
 
-while mainsel=$(zenity --title="LCD-Settings" --width="300" --height="180" --list --column "id" --column "Please select" --hide-column=1 --text="What do you want to do?" "bright" "Change LCD Brightness" "gamma" "Manage LCD Gamma"); do
+while mainsel=$(zenity --title="LCD-Settings" --width="300" --height="200" --list --column "id" --column "Please select" --hide-column=1 --text="What do you want to do?" "bright" "Change LCD Brightness" "gamma" "Manage LCD Gamma" "filter" "Select current video filter"); do
 
 case $mainsel in
 
@@ -71,6 +71,13 @@ case $mainsel in
     else
     echo $gamma > /sys/devices/platform/omap2_mcspi.1/spi1.1/gamma
     fi
+    fi;;
+
+    "filter")
+    if selection=$(head -1 /etc/pandora/conf/dss_fir/* | sed 's:==> ::' | sed 's: <==::' | sed '/^$/d' | zenity --width=700 --height=200 --title="Videofilter" --hide-column=1 --list --column "filter" --column "Videofilter" --text "Please select a videofilter" ); then
+      videofilter=$(basename $selection)
+      sudo /usr/pandora/scripts/op_videofir.sh $videofilter
+      zenity --info --title="Videofilter applied" --text "The videofilter has been applied." --timeout 6
     fi;;
 esac
 done
