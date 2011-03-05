e593814e89b56b114cb2006ed2002d058185bb8a
Michael Mrozek <EvilDragon@openpandora.de>
2011-03-05 02:17:14 +0100

    pandora-scripts: Moved some scripts back to pandora-libraries

--- recipes/pandora-system/pandora-scripts/pnd_hup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- 
-#HUP all apps who happen to look into one of the .desktop locations
- 
-PIDS=$(lsof +d /usr/share/applications  /usr/local/share/applications/ /home/*/Desktop /home/*/.applications /home/*/.local/share/applications | awk '!/PID/ {print $2 }' | uniq)
-for pid in $PIDS; do
-        kill -HUP $pid
-done
